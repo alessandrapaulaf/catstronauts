89d2eebb30bb221dd3f2857d544917057c06cc66
alessandrapaulaf <alessandra.paulafrancisco@gmail.com>
2023-06-15 13:03:15 -0300

    add mutation on server

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -9,6 +9,28 @@ const resolvers = {
       return dataSources.trackAPI.getTrack(id);
     }
   },
+  Mutation: {
+    incrementTrackViews: async (_, { id }, { dataSources }, info) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+  
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch(err) {
+        const { status, body } = err.extensions.response;
+        return {
+          code: status,
+          success: false,
+          message: body,
+          track: null
+        }
+      }
+    },
+  },
   Track: {
     author: ({ authorId }, _, { dataSources }, info) => {
       return dataSources.trackAPI.getAuthor(authorId);
